20b0c4903e6b70d32a5e7e18d9c4c08fefe74a71
kulikov-dev <kulikov.dev@gmail.com>
2017-06-09 16:56:37 +0500

    *first commit

---
+++ AwsWebApp1/Default.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using RH.Core.Helpers;
+using RH.WebCore;
+
+namespace AwsWebApp1
+{
+    public partial class WebForm1 : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        [WebMethod]
+        public static string LoadP(string a)
+        {
+            var creator = new ObjCreator();
+            return creator.Test();
+        }
+
+        [WebMethod]
+        public static string CropImage(string path)
+        {
+            try
+            {
+                CropHelper.WebCropImage(path, HttpContext.Current.Session.SessionID);
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            return HttpContext.Current.Session.SessionID;
+        }
+
+        [WebMethod]
+        public static string LoadModel(int currentType, string sessionID, string hairPath, string hairMaterialPath, string accessoriesPath, string accessoriesMaterialPath, string basePath, string baseMaterialPath, string addonPath1, string addonPath2, string addonPath3, string addonPath4, string addonMaterialPath, int ageValue, int fatValue, int smoothingValue, int size, string orderID)
+        {
+            try
+            {
+                var creator = new ObjCreator();
+                creator.CreateObj(currentType, sessionID, hairPath, hairMaterialPath, accessoriesPath, accessoriesMaterialPath, basePath, baseMaterialPath, addonPath1, addonPath2, addonPath3, addonPath4, addonMaterialPath, ageValue, fatValue, smoothingValue, size, orderID);
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            return HttpContext.Current.Session.SessionID;
+        }
+
+        [WebMethod]
+        public static string CheckFileExists(string path)
+        {
+            return FTPHelper.IsFileExists(path) ? HttpContext.Current.Session.SessionID : "bad";
+        }
+
+        [WebMethod]
+        public static double GetFaceAngle(string sessionId)
+        {
+            return ObjCreator.GetFaceAngle(sessionId);
+        }
+    }
+}
---
+++ AwsWebApp1/Global.asax.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.UI.WebControls;
+
+namespace AwsWebApp1
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            /*   var searchPath = @"C:\Windows\SysWOW64\inetsrv";
+               var oldPath = @"C:\Windows\system32\inetsrv";
+               var oldPath1 = @"C:\Windows\system32\inetsrv\bin";
+               var path = @"C:\Windows\SysWOW64\inetsrv\bin";
+               var q = AppDomain.CurrentDomain.BaseDirectory;      // C:\inetpub\wwwroot\          // тут лежит что надо! есть папка bin
+               var q1 = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"bin");
+               var p = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);  // тут хуйня, два файла лежит и все.  //C:\Windows\Microsoft.NET\Framework\v4.0.30319\Temporary ASP.NET Files\root\26676eb7\92c7e946\assembly\dl3\4efbb
+               var p1 = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\bin";*/
+
+            System.Environment.SetEnvironmentVariable("PATH", Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "bin"));
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
